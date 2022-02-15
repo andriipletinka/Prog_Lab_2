389ce36527d6a12b2f96c9a5367438dc054afc67
andriipletinka <andrii.pletinka@ucu.edu.ua>
2022-02-15 19:00:58 +0200

    Version 1

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "rZR3H9oqvIqRbqShofUBrk6iq",
+            "consumer_secret": "53aDithZMQFJrNUxNqgmfXWaduccsC4g7jKDS347ToZN03Wrph",
+            "token_key": "1492871794407219200-4iQh3tplRAvZJVxzYNw5qIhywB8O8T",
+            "token_secret": "YednOsIkzmKjR3KKYRf0wgsbWqrb99sDqbB83a1NxPdjg"}
